68ef2c1df4adf6797c044938b9876c83e6dde234
Husein Assidiq <huseinazidix12@gmail.com>
2020-12-19 17:43:49 +0800

    doc: add login

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+  providedIn: "root",
+})
+export class AuthService {
+  private _userIsAuthenticated = false;
+
+  constructor() {}
+
+  login() {
+    this._userIsAuthenticated = true;
+  }
+
+  logout() {
+    this._userIsAuthenticated = false;
+  }
+}
