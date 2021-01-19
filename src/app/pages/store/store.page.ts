0e9e6ef62fedf00ef425e31c4e4e8838e47132b2
Husein Assidiq <huseinazidix12@gmail.com>
2021-01-19 13:18:12 +0700

    doc: update store product with data

--- src/app/pages/store/store.page.ts
+++ src/app/pages/store/store.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 import { SegmentChangeEventDetail } from '@ionic/core';
+import { Plants } from 'src/app/interfaces/plants.interface';
+import { FirebaseService } from 'src/app/services/firebase.service';
 
 @Component({
   selector: 'app-store',
@@ -7,14 +10,38 @@ import { SegmentChangeEventDetail } from '@ionic/core';
   styleUrls: ['./store.page.scss'],
 })
 export class StorePage implements OnInit {
+  plant: any = [];
+  plantList: any = [];
+  plantData: Plants;
+  segment: string;
 
-  constructor() { }
+  constructor(
+    private firebaseService: FirebaseService,
+    private loadingCtrl: LoadingController
+  ) {}
 
   ngOnInit() {
+    this.firebaseService.readPlant().subscribe((data) => {
+      this.plantList = data.map((e) => {
+        return {
+          id: e.payload.doc.id,
+          name: e.payload.doc.data()['name'],
+          price: e.payload.doc.data()['price'],
+          description: e.payload.doc.data()['description'],
+          img: e.payload.doc.data()['img'],
+        };
+      });
+    });
   }
 
   onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>) {
-    console.log(event.detail)
+    this.segment = event.detail.value;
+    console.log(event.detail);
   }
 
+  onDelete(id: string) {
+    this.loadingCtrl.create();
+    this.firebaseService.deletePlant(id);
+    this.loadingCtrl.dismiss();
+  }
 }
